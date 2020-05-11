7791a7e56b0091e6aa0a1e6d53a50b5ff00f4914
Filipe Mota <drete457@gmail.com>
2020-05-11 16:58:43 +0000

    Bug Fixes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count -1
+    });
+  };
+
+  render() {
+    return (
+      <div clasname="App">
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
