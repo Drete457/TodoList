50e259eaa05e3bb22cf6b7848648a845d5fe963a
Filipe Mota <Drete457@gmail.com>
2020-05-18 14:46:37 +0000

    Final Page

---
+++ precache-manifest.54ff8cfa7fc1128626057fe9909dd055.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c175cbc69b1efb0446f0efc98157abc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42dd8f3eb0498f9c8eb5",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "0d07b52a0cf7b522b6da",
+    "url": "/static/css/main.b5b28cf8.chunk.css"
+  },
+  {
+    "revision": "42dd8f3eb0498f9c8eb5",
+    "url": "/static/js/2.905b12a9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.905b12a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d07b52a0cf7b522b6da",
+    "url": "/static/js/main.7e09a6ff.chunk.js"
+  },
+  {
+    "revision": "8f41a90e8619d2bb09e8",
+    "url": "/static/js/runtime-main.11dfc852.js"
+  },
+  {
+    "revision": "77bf491711febd4ab7df20154a64c732",
+    "url": "/static/media/trash.77bf4917.png"
+  }
+]);
